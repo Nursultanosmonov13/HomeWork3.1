6a9ca4a2746e768bbe20fadb6895a6b03f10963a
Nursultanosmonov13 <nursultanosmonov93@gmail.com>
2021-01-28 21:38:01 +0600

    Поменял код

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,34 @@
 package com.company;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Main {
+
     public static void main(String[] args) {
-        // double[] myArrays = {1.1, 12.1, 9.5, -6.3, -3.12, 23.1, 5.14, 3.2, 3.3, 10.6, -12.9, 6.8, 2.9, 8.3, 2.2};
-        //double result = 0;
-        int [] myArrays ={8, -2, -4, 2, 3, 6};
-        int sum = 0;
+        double[] myArrey = {1.1, 12.1, 9.5, -6.3, -3.12, 23.1, 5.14, 3.2, 3.3, 10.6, 12.9, 6.8, 2.9, 8.3};
+        double sumNumber = 0;
+        double result = 0;
         int count = 0;
-        int result = 0;
-        boolean firstMinusElementFound = false;
-        for (int item : myArrays) {
-            if (item < 0){
-                firstMinusElementFound = true;
-                // System.out.println("firstMinusElementFound " + firstMinusElementFound);
+        boolean negatNumber = false;
+        for (double temp : myArrey) {
+            if (temp < 0) {
+                negatNumber = true;
+                //System.out.println(negatNumber);
                 continue;
             }
+            if (negatNumber) {
+                if (temp > 0) {
+                    //System.out.println("temp " + temp);
+                    //sumNumber = temp;
+                    count = count + 1;
+                    sumNumber = sumNumber + temp;
+                    result = sumNumber / count;
 
-                if (item > 0){
-                    count++;
-                    sum = sum + item;
                 }
 
-        }
-        System.out.println("sum "+sum);
-        System.out.println("count "+count);
-        result = sum/count;
-        System.out.println("result "+result);
-    }
+            }
 
+        }
+        System.out.println("count - " + count);
+        System.out.println("sumNumber - " + sumNumber);
+        System.out.println("Резултат - " + result);
+    }
 }
